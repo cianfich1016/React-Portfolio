2e16f9d65359c9ccbfa1ac622d36c3d2bb0173e6
Rachel Cianfichi <cianfich@gmail.com>
2021-11-16 18:03:19 -0500

    Adjusted routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Portfolio from "./components/Portfolio"
+import Contact from "./components/Contact"
+import Resume from "./components/Resume"
 import "./components/style.css"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
-
+          <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              <Route path="/resume">
+                <Resume />
+              </Route>
+              <Route path="/">
+                <About />
+              </Route>
+          </Switch> 
       <Footer />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Navigation from "./Navigation"
 import About from "./About"
 import Portfolio from "./Portfolio"
 import Contact from "./Contact"
 import Resume from "./Resume"
 
-function Header(){
-    const [currentPage, setCurrentPage] = useState('About');
-
-    useEffect(() => {
-        // check the url 
-        if (window.location.pathname === '/about') {
-            setCurrentPage('About')
-        }else if (window.location.pathname === '/portfolio') {
-            setCurrentPage('Portfolio')
-        }else if (window.location.pathname === '/contact') {
-            setCurrentPage('Contact')
-        }else if (window.location.pathname === '/resume') {
-            setCurrentPage('Resume')}
-    }, [])
-    
-    const renderPage = () => {
-        if (currentPage === 'About') {
-          return <About />;
-        }
-        if (currentPage === 'Portfolio') {
-          return <Portfolio />;
-        }
-        if (currentPage === 'Contact') {
-          return <Contact />;
-        }
-        return <Resume />;
-      };
-    
-      const handlePageChange = (page) => setCurrentPage(page);
-    
+function Header(){    
     return(
         <div >
-            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
+            <Navigation />
         </div>
-    )
+    );
 }
 
 export default Header;
