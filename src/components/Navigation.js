1beca938f1a9e6d631cd134aa91c3e82ab579c34
Rachel Cianfichi <cianfich@gmail.com>
2021-11-16 17:21:06 -0500

    troubleshoot github connection

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -30,16 +30,16 @@ function Navigation({currentPage, handlePageChange}){
                 <div style = {styles.bar} class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a style = {styles.nav} class="nav-link" href="/about"onClick={(event) => {event.preventDefault(); handlePageChange('About'); window.history.pushState("About", "About", "/about")}} onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Me</a>
+                            <a style = {styles.nav} class="nav-link" href="/about" className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Me</a>
                         </li>
                         <li class="nav-item">
-                            <a style = {styles.nav} class="nav-link" href="/portfolio" onClick={(event) => {event.preventDefault(); handlePageChange('Portfolio'); window.history.pushState("Portfolio", "Portfolio", "/portfolio")}} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a>
+                            <a style = {styles.nav} class="nav-link" href="/portfolio" className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a>
                         </li>
                         <li class="nav-item">
-                            <a style = {styles.nav} class="nav-link" href="/contact" onClick={(event) => {event.preventDefault(); handlePageChange('Contact'); window.history.pushState("Contact", "Contact", "/contact")}} onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact Me</a>
+                            <a style = {styles.nav} class="nav-link" href="/contact" className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact Me</a>
                         </li>
                         <li class="nav-item">
-                            <a style = {styles.nav} class="nav-link" href="/resume" onClick={(event) => {event.preventDefault(); handlePageChange('Resume'); window.history.pushState("Resume", "Resume", "/resume")}} onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
+                            <a style = {styles.nav} class="nav-link" href="/resume" className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
                         </li>
                     </ul>
                 </div>
